ad250075078e75bb5fcadcd3d1182b60f363cd3e
abhinav dev <39181811+adev101@users.noreply.github.com>
2024-10-12 15:58:20 +0530

    Create jenkins.sh

---
+++ shellscripts/jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo su
+apt update -y && apt upgrade -y && apt install openjdk-17-jdk -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y 
+sudo apt install jenkins -y
+systemctl start jenkins
